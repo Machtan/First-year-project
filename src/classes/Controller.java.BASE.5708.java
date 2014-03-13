0073a2b51a7d74ada1cc01cfbc73eb733af5ef3a
Daniel <dafr@itu.dk>
2014-03-13 15:00:37 +0100

    Merged

---
+++ src/classes/Controller.java.BASE.5708.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package classes;
+
+/**
+ *
+ * @author Isabella
+ */
+public class Controller {
+    private Model model;
+    private View view;
+    private Loader loader;
+
+    public Controller(){
+       model = new Model(Loader.loadIntersections("resources/intersections.txt"), 
+               Loader.loadRoads("resources/roads.txt"));
+       view = new View();
+       view.setLines(model.getLines());
+       
+    }
+       
+    public static void main(String[] args){
+        //Run program
+        Controller controller = new Controller();
+        
+        
+        
+        
+    }
+    
+    
+}
